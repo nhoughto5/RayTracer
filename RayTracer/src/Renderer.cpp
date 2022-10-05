02898ce6a47d3bd5fac41fcf6f5b9ad335020b1b
Nick Houghton <nhoughto5@gmail.com>
2022-10-05 14:21:13 -0700

    Basic Light working

--- RayTracer/src/Renderer.cpp
+++ RayTracer/src/Renderer.cpp
@@ -1,5 +1,17 @@
 #include "Renderer.h"
 
+namespace Utils
+{
+    static uint32_t ConvertToRGBA(const glm::vec4& color)
+    {
+        uint8_t r = (uint8_t)(color.r * 255.0f);
+        uint8_t g = (uint8_t)(color.g * 255.0f);
+        uint8_t b = (uint8_t)(color.b * 255.0f);
+        uint8_t a = (uint8_t)(color.a * 255.0f);
+
+        return (a << 24) | (b << 16) | (g << 8) | r;
+    }
+}
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
     if (m_FinalImage)
@@ -27,17 +39,21 @@ void Renderer::Render()
         {
             glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight()};
             coord = (coord * 2.0f) - 1.0f;
-            m_ImageData[(y * m_FinalImage->GetWidth()) + x] = PerPixel(coord);
+            glm::vec4 color = PerPixel(coord);
+            color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+            m_ImageData[(y * m_FinalImage->GetWidth()) + x] = Utils::ConvertToRGBA(color);
         }
     }
 
     m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
     glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
     glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+    glm::vec3 lightPosition(-1, -1, -1);
+    glm::vec3 center(0, 0, 0);
     rayDirection = glm::normalize(rayDirection);
     float radius = 0.5f;
 
@@ -48,10 +64,20 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
     // Quadratic discriminate
     float d = b * b - 4.0f * a * c;
 
-    if (d >= 0.0f)
+    if (d < 0.0f)
     {
-        return 0xffff00ff;
+        // No intersection
+        return glm::vec4(0, 0, 0, 1.0f);
     }
 
-    return 0xff000000;
+    float t0 = (-b + glm::sqrt(d)) / (2.0f * a);
+    float closestT = (-b - glm::sqrt(d)) / (2.0f * a);
+
+    glm::vec3 hitPoint = rayOrigin + rayDirection * closestT;
+    glm::vec3 normal = glm::normalize(hitPoint - center);
+
+    float light = glm::max(glm::dot(normal, -glm::normalize(lightPosition)), 0.0f);
+
+    glm::vec3 sphereColor(1, 0, 1);
+    return glm::vec4(sphereColor * light, 1.0f);
 }
